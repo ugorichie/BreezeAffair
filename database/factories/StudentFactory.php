17595c8222c0d9ed12da6b5d4bd76b3d7ee55129
oguchi ugochukwu <ugoebukaoguchi@gmail.com>
2024-02-15 16:51:16 +0100

    edited the factories to store dummy data suitable to the model ways

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
@@ -17,7 +19,12 @@ class StudentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'studentreg'=> fake()->unique(),
+            //'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
         ];
     }
 }
